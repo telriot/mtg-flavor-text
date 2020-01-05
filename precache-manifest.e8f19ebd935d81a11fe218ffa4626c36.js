a8360c298fcd3c50d08c7734d83b6e523144fbd2
Benjo <beniamino.tartarini@gmail.com>
2020-01-05 12:18:05 +0900

    Updates

---
+++ precache-manifest.e8f19ebd935d81a11fe218ffa4626c36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7508f1f2d844298e5d42a58fb5c07666",
+    "url": "/mtg-flavor-text/index.html"
+  },
+  {
+    "revision": "a92cdc5371093743837b",
+    "url": "/mtg-flavor-text/static/js/2.fb8d3aa0.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/mtg-flavor-text/static/js/2.fb8d3aa0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2c6307375468d5e44fa",
+    "url": "/mtg-flavor-text/static/js/main.c000bbf2.chunk.js"
+  },
+  {
+    "revision": "248db3599b98f6ceaee4",
+    "url": "/mtg-flavor-text/static/js/runtime-main.9dad6282.js"
+  }
+]);
